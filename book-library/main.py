ab4dcc8fffd9dd4ac7bced4606a7d940f966dff4
mofox0919 <mofox0919@gmail.com>
2025-06-12 14:03:23 -0400

    new stuff

---
+++ book-library/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import date
+
+app = FastAPI()
+books = []
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    year: date
+
+class BookInput(BaseModel):
+    title: str
+    author: str
+    year: date
+
+@app.get("/books/")
+def getBooks():
+    return books
+
+@app.get("/books/{id}")
+def getBook():
+    return books[id]
+
+@app.delete("/books/{id}")
+def deleteBook():
+    del books[id]
+
+@app.post("/books/")
+def addBook(item: BookInput):
+    new_book = Book(
+        id = len(books) + 1,
+        title=item.title,
+        author=item.author,
+        year=item.year
+    )
+    books.append(new_book)
+    return new_book 
+
+@app.post("/books/{id}")
+def addBook(item: BookInput):
+    books[id].title = item.title
+    books[id].author = item.author
+    books[id].year = item.year
